7f5e5653a8c4c32b3af036b06a222db19c7bf4c6
gkshri <gokulshri@outlook.com>
2021-02-10 14:44:06 +0530

    --------

--- keyvalue.rb
+++ keyvalue.rb
@@ -6,3 +6,14 @@ puts "keys #{key.join(', ')}"
 kindadict.each do |a, b|  
     puts "#{a.capitalize} --- #{b.join(', ')}"
 end
+
+books = ["Design as Art", "Anathem", "Shogun"]  
+authors = ["Bruno Munari", "Neal Stephenson", "James Clavell"]  
+# fill in code that will create a Hash that looks like:  
+#   {  
+    #     :bruno => "Design as Art",  
+    #     :neal => "Anathem",  
+    #     ...  
+    #   }
+library={}
+tmp = books.zip(author).map{}
