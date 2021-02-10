00e3af65404d9eb7f7d09007a8c8ccb60439838a
gkshri <gokulshri@outlook.com>
2021-02-10 13:17:24 +0530

    push 1

--- todos.rb
+++ todos.rb
@@ -4,5 +4,12 @@ puts "\n\n\n"
 
 puts "This is your next todo"
 puts next_todo
-puts "[] #{10.times{puts next_todo}}" # String interpolation (any ruby expression)
+# puts "[] #{10.times{puts next_todo}}" # String interpolation (any ruby expression)
 #first change
+age = 21
+puts "my age is " + age.to_s
+puts "my age is #{age}"
+#n=5
+n=5.0
+m=2
+puts "ans #{n/m}"
