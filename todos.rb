124bd5a0f531f836702568ba7907f5972b477dcb
gkshri <gokulshri@outlook.com>
2021-02-10 12:55:39 +0530

    first file

---
+++ todos.rb
@@ -0,0 +1,7 @@
+puts "enter your next todo" # hello world
+next_todo = gets
+puts "\n\n\n"
+
+puts "This is your next todo"
+puts next_todo
+puts "[] #{10.times{puts next_todo}}" # String interpolation (any ruby expression)
